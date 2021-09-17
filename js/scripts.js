c6d03e0f259b9e2febd0b0b3e36c72096733f7ad
Leonidas Roubas <leonidasroubas@gmail.com>
2021-09-16 22:22:53 -0500

    Login-page&Dashboard

--- js/scripts.js
+++ js/scripts.js
@@ -21,4 +21,43 @@ $(function() {
     $("#loginButton").click(function() {
         $('#loginModal').modal('show');
     });
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    // $('#dismiss, .overlay').on('click', function () {
+    //     // hide sidebar
+    //     $('#sidebar').removeClass('active');
+    //     // hide overlay
+    //     $('.overlay').removeClass('active');
+    // });
+
+    // $('#sidebarCollapse').on('click', function () {
+    //     // open sidebar
+    //     $('#sidebar').addClass('active');
+    //     // fade in the overlay
+    //     $('.overlay').addClass('active');
+    //     $('.collapse.in').toggleClass('in');
+    //     $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    // });
+    
+    function toggleResetPswd(e){
+        e.preventDefault();
+        $('#logreg-forms .form-signin').toggle() // display:block or none
+        $('#logreg-forms .form-reset').toggle() // display:block or none
+    }
+    
+    function toggleSignUp(e){
+        e.preventDefault();
+        $('#logreg-forms .form-signin').toggle(); // display:block or none
+        $('#logreg-forms .form-signup').toggle(); // display:block or none
+    }
+    
+    $(()=>{
+        // Login Register Form
+        $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
+        $('#logreg-forms #cancel_reset').click(toggleResetPswd);
+        $('#logreg-forms #btn-signup').click(toggleSignUp);
+        $('#logreg-forms #cancel_signup').click(toggleSignUp);
+    })
 });
